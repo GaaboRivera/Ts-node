46feda162a54c274f097c6ff1fd678220ff832ac
GaaboRivera <gabo.grm27@gmail.com>
2022-10-25 19:18:43 -0500

    genericos

--- src/classes/Hero.ts
+++ src/classes/Hero.ts
@@ -1,13 +1,21 @@
-import powers, { Power } from '../data/powers';
+import powers from '../data/powers';
+
 
 export class Hero {
     constructor(
         public name: string,
         public powerId: number,
         public age: number
-        ){}
+    ) {}
 
-    get power():string {
-        return powers.find(power=> power.id === this.powerId)?.desc || 'not found'
+    get power():string { 
+        return powers.find( power => power.id === this.powerId )?.desc || 'not found';
     }
 }
+
+export class Hero2 { }
+export class Hero3 { }
+export class Hero4 { }
+
+export const PI = 3.1416;
+export const miNombre = 'Fernando';
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
-import { Hero } from "./classes/Hero";
+// import { Hero } from "./classes/Hero";
+import { getPokemon } from './generics/get-pokemon';
 
-const iroman = new Hero('Gabo', 1, 33)
+// const iroman = new Hero('Gabo', 1, 33)
 
-console.log(iroman);
-console.log(iroman.power);
-
-
+// console.log(iroman);
+// console.log(iroman.power);
 
 
+
+
+
+
+
+
+getPokemon(4)
+    .then( pokemon => console.log( pokemon ) )
+    .catch( error => console.error( error ) )
+    .finally( () => console.log('Fin de getPokemon')  )
