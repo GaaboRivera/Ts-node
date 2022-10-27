f0a33e54ca3b9bad2559e87a50acd46092a10cbc
GaaboRivera <gabo.grm27@gmail.com>
2022-10-27 10:43:47 -0500

    decoradores

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,24 @@
 // import { Hero } from "./classes/Hero";
-import { getPokemon } from './generics/get-pokemon'
+// import { getPokemon } from './generics/get-pokemon'
 
-// const iroman = new Hero('Gabo', 1, 33)
+// // const iroman = new Hero('Gabo', 1, 33)
 
-// console.log(iroman);
-// console.log(iroman.power);
+// // console.log(iroman);
+// // console.log(iroman.power);
 
-getPokemon(4)
-  .then((pokemon) => console.log(pokemon))
-  .catch((error) => console.error(error))
-  .finally(() => console.log('Fin de getPokemon'))
+// getPokemon(4)
+//   .then((pokemon) => console.log(pokemon))
+//   .catch((error) => console.error(error))
+//   .finally(() => console.log('Fin de getPokemon'))
+
+//* DECORADORES
+import { Pokemon } from './decorators/pokemon-class'
+
+const charmander = new Pokemon('Charmander')
+
+// (Pokemon.prototype as any).customName = 'Pikachu'
+
+// console.log(charmander.savePokemonToDB(50));
+// charmander.savePokemonToDB(10);
+charmander.publicApi = 'https://fernando-herrera.com'
+console.log(charmander)
